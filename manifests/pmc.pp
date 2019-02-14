70029c30b8b38298a49b610c6004fdc9daa8c287
Artur Faizullin <artur@thehyve.nl>
2019-02-14 12:37:45 +0100

    fix missing :

--- manifests/pmc.pp
+++ manifests/pmc.pp
@@ -1,7 +1,7 @@
 class ansible::pmc(
     String $staging_dir = '/srv/pmc',
 ) {
-    file { '/var/lib/playbooks/pmc_vars.yml'
+    file { '/var/lib/playbooks/pmc_vars.yml':
         ensure => file,
         content => template('ansible/pmc_vars.yml.erb'),
     }
