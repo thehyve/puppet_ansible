0f6c3f2e5d004421e30d10bba3b5a33b809a51d8
Artur Faizullin <artur@thehyve.nl>
2019-02-14 13:23:50 +0100

    copy all pmc playbooks

--- manifests/pmc.pp
+++ manifests/pmc.pp
@@ -10,4 +10,29 @@ class ansible::pmc(
             "puppet:///modules/ansible/cbio_dump.yml",
         ],
     }
+    file { '/var/lib/playbooks/dexec.yml':
+        source => [
+            "puppet:///modules/ansible/dexec.yml",
+        ],
+    }
+    file { '/var/lib/playbooks/keycloak_dump.yml':
+        source => [
+            "puppet:///modules/ansible/keycloak_dump.yml",
+        ],
+    }
+    file { '/var/lib/playbooks/rotate.yml':
+        source => [
+            "puppet:///modules/ansible/rotate.yml",
+        ],
+    }
+    file { '/var/lib/playbooks/save_notify.yml':
+        source => [
+            "puppet:///modules/ansible/save_notify.yml",
+        ],
+    }
+    file { '/var/lib/playbooks/transmart_dump.yml':
+        source => [
+            "puppet:///modules/ansible/transmart_dump.yml",
+        ],
+    }
 }
